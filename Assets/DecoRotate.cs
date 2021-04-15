167afeb3cf1a425406522f1194c4d5ec90e31140
Hugo_A <hugoagnola@gmail.com>
2021-04-15 06:27:27 -0700

    wip

---
+++ Assets/DecoRotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecoRotate : MonoBehaviour
+{
+    public float speed = 5f;
+
+    // Update is called once per frame
+    public void FixedUpdate()
+    {
+        transform.Rotate((Vector3.up / 2), - speed * Time.deltaTime);
+    }
+}
